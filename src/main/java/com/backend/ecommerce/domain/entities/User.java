3d0034ce6426314cf3571235c02cf2bf7c6b372e
Yousif FJ <47510107+Yousif-FJ@users.noreply.github.com>
2024-09-12 18:50:25 +0300

    Remove unnecessary column definition in User

--- src/main/java/com/backend/ecommerce/domain/entities/User.java
+++ src/main/java/com/backend/ecommerce/domain/entities/User.java
@@ -43,7 +43,7 @@ public class User implements UserDetails {
     @Column(name="password")
     private String password;
 
-    @Column(name = "type", columnDefinition = "enum('ADMIN', 'USER')")
+    @Column(name = "type")
     @Enumerated(EnumType.ORDINAL)
     private UserRole userRole;
 
